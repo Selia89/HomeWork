3ce0a08d3b00f808f962c16438b688bc36f47302
Selia_89 <selia_89@mail.ru>
2018-09-14 23:39:27 +0300

    Task4

---
+++ Task4/1.js
@@ -0,0 +1,53 @@
+class Person { // Базовый класс
+    constructor(name, gender) { // Конструктор
+        this._name = name; // Создание и инициализация поля
+        this._friends = [ ];
+        this._gender = gender;
+    }
+  
+    setFriend(other) {
+    if (this._friends.indexOf(other) == -1) {
+            this._friends.push(other);
+            other._friends.push(this);
+        }
+    }
+    
+    hasFriend(other) {
+        return this._friends.includes(other);
+    }
+
+    marry(pers) {
+        if(this._spouse === "single" && this._gender !== pers._gender) {
+            this._spouse = pers;
+            pers._spouse = this;
+        } else {
+            console.log("Однополые браки и многожонство у нас не приняты");
+        }
+    }
+
+}
+
+class Man extends Person {
+    constructor(name, _friends) {
+        super(name, _friends);
+        this._gender = "man";
+        this._spouse = "single";
+    }
+}
+
+class Woman extends Person {
+    constructor(name, _friends) {
+        super(name, _friends);
+        this._gender = "woman";
+        this._spouse = "single";
+    }
+}
+
+const ivan = new Man("Ivan");
+const anna = new Woman("Anna");
+
+ivan.setFriend(anna);
+ivan.marry(anna);
+
+console.log(ivan);
+console.log(anna);
